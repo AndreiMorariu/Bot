4c1660c3961fc5981f716f13c56f8f8eaffc4afd
AndreiMorariu <andreimorariu4140@gmail.com>
2023-01-09 16:33:58 +0200

    Initial commit

---
+++ commands/ping.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("vorbeste")
+    .setDescription("Replies according to the user"),
+  async execute(interaction) {
+    if (interaction.user.username === "AndreiZk")
+      await interaction.reply("Da, maestre");
+    else await interaction.reply("Nu raspund la sclavi");
+  },
+};
